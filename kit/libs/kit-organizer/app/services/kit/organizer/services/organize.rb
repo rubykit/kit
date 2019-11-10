462b57fcf3652a60ab5ecf355f3e3b0f5db3ab19
Nathan Appere <nathan.appere@gmail.com>
2019-11-10 20:39:49 +0100

    First Yard attempt, added some css to make the default theme OK

--- kit/libs/kit-organizer/app/services/kit/organizer/services/organize.rb
+++ kit/libs/kit-organizer/app/services/kit/organizer/services/organize.rb
@@ -1,23 +1,24 @@
 module Kit::Organizer::Services
+  # Based on Railway programming, Interactor.
   module Organize
 
     Operation = Or[Callable, Symbol, Tupple[Or[String, Symbol], Or[String, Symbol]]]
 
     # Run a `list` of `operations` (callable) in order. Each results update the initial `ctx` which is then sent to the next operation.
-    # An `operation` needs to be a callable, but it can be resolved from other format (look at `to_callable`)
-    # Note: Every operation is expected to return a tupple of the format `[:ok]` or `[:error]` with an optional context update (`[:ok, { new_ctx_key: 'value' }]`, [:errors, { errors: [{ detail: 'Error explaination' }], }]`). If an `:error` tupple is returned, the next operations are canceled and `call` will return.
+    # An `operation` needs to be a callable, but it can be resolved from other format (see #to_callable)
+    # Note: Every operation is expected to return a tupple of the format `[:ok]` or `[:error]` with an optional context update (`[:ok, { new_ctx_key: 'value' }]`, `[:errors, { errors: [{ detail: 'Error explaination' }], }]`). If an `:error` tupple is returned, the next operations are canceled and `call` will return.
     # @param list An array of operations (callables) that will be called in order
     # @param ctx A hash containing values to send to the operations (callables). It will be updated after every operation.
     # @param expose Allows to slice
     # @return The updated context.
-    contract Hash[list: Array.of(Operation), ctx: Optional[Hash], expose: Optional[Or[Hash[ok: Array], Hash[:error, Array]]] => ResultTupple
+    # contract Hash[list: Array.of(Operation), ctx: Optional[Hash], expose: Optional[Or[Hash[ok: Array], Hash[:error, Array]]] => ResultTupple
     def self.call(list:, ctx: {}, expose: nil)
       ctx    = ctx.dup
       status = :ok
 
       begin
         list
-          .map { to_callable(callable: @1) }
+          .map { |el| to_callable(callable: el) }
           .each do |callable|
             local_ctx = generate_callable_ctx(callable: callable, ctx: ctx)
 
@@ -40,7 +41,6 @@ module Kit::Organizer::Services
               break
             end
           end
-        end
       #rescue StandardException => e
       #  status = :error
       #  # Todo: use event bus to notify error handlers ?
@@ -58,6 +58,7 @@ module Kit::Organizer::Services
     end
 
     # Sanitizes returned errors, if any.
+    # @private
     # @note Enable simpler error return format from an organized callable.
     # @example Returning an error as a string
     #    sanitize_result([:error, 'Error details']) => [:error, { errors: [{ detail: 'Error details' }] }]
@@ -65,7 +66,7 @@ module Kit::Organizer::Services
     #    sanitize_result([:error, { detail: 'Error details' }]) => [:error, { errors: [{ detail: 'Error details' }] }]
     # @example Returning an array of errors in the two former formats
     #    sanitize_result([:error, ['Error1 detail', { detail: 'Error2 details' }]) => [:error, { errors: [{ detail: 'Error1 details' }, { detail: 'Error2 details' }] }]
-    contract ResultTupple => ResultTupple
+    # contract ResultTupple => ResultTupple
     def self.sanitize_errors(result:)
       status, ctx = result
 
@@ -78,7 +79,7 @@ module Kit::Organizer::Services
           ctx = { errors: [ctx] }
         end
       elsif ctx.is_a?(Array)
-        ctx = { errors: ctx.map { @1.is_a?(String) ? { detail: el } : @1 } }
+        ctx = { errors: ctx.map { |el| el.is_a?(String) ? { detail: el } : el } }
       end
 
       [status, ctx]
@@ -100,13 +101,14 @@ module Kit::Organizer::Services
     end
 
     # Ensures every action is a callable.
-    # @note Accepted format are `['Module', 'singleton_method_name']` or `:method_identifier` (in this case Kit::Organizer::Store is used to get the real callable)
+    # @private
+    # @note Accepted format are `['Module', 'singleton_method_name']` or `:method_identifier` (in this case `Kit::Organizer::Store` is used to get the real callable)
     # @example Generating a callable from a tupple
     #    to_callable(callable: ['AuthenticationModule', 'sign_in']) => Proc(AuthenticationModule#sign_in)
     # @example Generating a callable from a tupple
     #    Kit::Organizer::Store.register(id: :login, target: AuthenticationModule.method(:sign_in)) => true
     #    to_callable(callable: :login) => Proc(AuthenticationModule#sign_in)
-    contract Hash[callable: Or[Callable, Symbol, Array.size(2)]]
+    # contract Hash[callable: Or[Callable, Symbol, Array.size(2)]]
     def self.to_callable(callable:)
       if callable.is_a?(Array)
         class_object, method_name = callable
@@ -122,11 +124,12 @@ module Kit::Organizer::Services
     end
 
     # Extract needed key from the organizer context to send them to the callable.
+    # @private
     # @note This id done by using introspection on the callable.
     # @example
     #    generate_callable_ctx(callable: ->(a:), { a: 1, b: 2, c: 3 }) => { a: 1 }
     #    generate_callable_ctx(callable: ->(a:, **), { a: 1, b: 2, c: 3 }) => { a: 1, b: 2, c: 3 }
-    contract Hash[callable: Callable, ctx: Hash]
+    # contract Hash[callable: Callable, ctx: Hash]
     def self.generate_callable_ctx(callable:, ctx:)
       parameters = Kit::Contract::Services::SignatureMatcher.get_parameters(callable)
       by_type    = parameters.group_by { |el| el[0] }
@@ -138,14 +141,15 @@ module Kit::Organizer::Services
       if by_type[:keyrest]
         ctx.dup
       elsif by_type[:keyreq] || by_type[:key]
-        ctx.slice(*(by_type[:keyreq].map { @1[1] } + by_type[:key].map { @1[1] }))
+        ctx.slice(*(by_type[:keyreq].map { |el| el[1] } + by_type[:key].map { |el| el[1] }))
       else
         {}
       end
     end
 
     # Display debug information when ENV['LOG_ORGANIZER'] is set
-    def _log(txt, color = nil)
+    # @private
+    def self._log(txt, color = nil)
       env_value = ENV['LOG_ORGANIZER']
       return if !env_value || env_value == ''
 
