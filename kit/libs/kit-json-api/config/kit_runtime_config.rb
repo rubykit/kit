09ddc3e9cb8647398a140a352b5d5df98c7225ea
Nathan Appere <nathan.appere@gmail.com>
2020-04-20 20:45:19 +0200

    Kit::DummyAppContainer - Adding DB support

--- kit/libs/kit-json-api/config/kit_runtime_config.rb
+++ kit/libs/kit-json-api/config/kit_runtime_config.rb
@@ -11,6 +11,7 @@ KIT_APP_PATHS['GEM_SPEC_ROOT']   = File.expand_path('../spec/dummy', __dir__)
 KIT_APP_PATHS['GEM_SPEC_APP']    = File.expand_path('../spec/dummy/app', __dir__)
 KIT_APP_PATHS['GEM_SPEC_LIB']    = File.expand_path('../spec/dummy/lib', __dir__)
 KIT_APP_PATHS['GEM_SPEC_ROUTES'] = File.expand_path('../spec/dummy/config/routes.rb', __dir__)
+KIT_APP_PATHS['GEM_SPEC_DB']     = File.expand_path('../spec/dummy/db', __dir__)
 KIT_APP_PATHS['GEM_SPEC_INITIALIZERS'] = [
   File.expand_path('../spec/dummy/config/initializers/eager_load_controllers.rb', __dir__),
 ]
