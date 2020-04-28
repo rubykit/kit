07ea4c0ef436b483e07f07d6c70095b35639faa4
Nathan Appere <nathan.appere@gmail.com>
2020-04-28 17:05:55 +0200

    YardKit - Fix highlighting

--- kit/libs/yard-kit/assets/src/js/html.js
+++ kit/libs/yard-kit/assets/src/js/html.js
@@ -8,12 +8,11 @@ import hljs from 'highlight.js/lib/highlight'
 import bash from 'highlight.js/lib/languages/bash'
 import css from 'highlight.js/lib/languages/css'
 import diff from 'highlight.js/lib/languages/diff'
-import erlang from 'highlight.js/lib/languages/erlang'
-import erlangRepl from 'highlight.js/lib/languages/erlang-repl'
 import http from 'highlight.js/lib/languages/http'
 import javascript from 'highlight.js/lib/languages/javascript'
 import json from 'highlight.js/lib/languages/json'
 import markdown from 'highlight.js/lib/languages/markdown'
+import ruby from 'highlight.js/lib/languages/ruby'
 import sql from 'highlight.js/lib/languages/sql'
 import xml from 'highlight.js/lib/languages/xml'
 
@@ -53,12 +52,11 @@ $(() => {
   hljs.registerLanguage('bash', bash)
   hljs.registerLanguage('css', css)
   hljs.registerLanguage('diff', diff)
-  hljs.registerLanguage('erlang', erlang)
-  hljs.registerLanguage('erlang-repl', erlangRepl)
   hljs.registerLanguage('http', http)
   hljs.registerLanguage('javascript', javascript)
   hljs.registerLanguage('json', json)
   hljs.registerLanguage('markdown', markdown)
+  hljs.registerLanguage('ruby', ruby)
   hljs.registerLanguage('sql', sql)
   hljs.registerLanguage('xml', xml)
 
