cf94e30aa9eb85f13a31575b2be506cc042b4f3a
Nathan Appere <nathan.appere@gmail.com>
2020-11-01 10:58:21 +0100

    Chore: fixing specs

--- domains/kit-auth/spec/support/database_cleaner.rb
+++ domains/kit-auth/spec/support/database_cleaner.rb
@@ -2,7 +2,8 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
 
-  config.before(:suite) do
+  # TODO: optimize this per category of test. Maybe add a `:db` flag?
+  config.before(:each) do
     [Kit::Auth::Models::WriteRecord].each do |model|
       cleaner = DatabaseCleaner[:active_record, { model: model }]
       cleaner.clean_with(:truncation)
