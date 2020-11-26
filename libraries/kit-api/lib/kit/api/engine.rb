440cc7336f27157a2498121cd8e635ccade0a9c0
Nathan Appere <nathan.appere@gmail.com>
2020-11-26 18:18:58 +0100

    Kit::Api - Update json api MIME types initializers

--- libraries/kit-api/lib/kit/api/engine.rb
+++ libraries/kit-api/lib/kit/api/engine.rb
@@ -5,8 +5,19 @@ class Kit::Api::Engine < ::Rails::Engine
 
   dir = __dir__
 
-  initializer :spec_append_migrations do |app|
+  initializer 'kit-api.spec_append_migrations' do |app|
     app.config.paths['db/migrate'] << File.expand_path('../../../spec/dummy/db/migrate', dir)
+  end
+
+  initializer 'kit-api.jsonapi_mime_types' do
+    media_type = 'application/vnd.api+json'.freeze
+
+    Mime::Type.register(media_type, :json_api)
+
+    parser = ->(body) { Oj.safe_load(body) }
+
+    #Note: `ActionDispatch::IntegrationTest.register_encoder` does not seem to work
+    ActionDispatch::Request.parameter_parsers[:json_api] = parser
   end
 
 end
--- libraries/kit-router/lib/kit/router/engine.rb
+++ libraries/kit-router/lib/kit/router/engine.rb
@@ -13,11 +13,4 @@ class Kit::Router::Engine < ::Rails::Engine
     ActionView::Base.include Kit::Router::ViewHelpers::HttpRoutes
   end
 
-  # TODO: move this to an kit-api gem ?
-  initializer 'kit-router.mime_types' do
-    media_type = 'application/vnd.api+json'.freeze
-    Mime::Type.register(media_type, :jsonapi)
-    Mime::Type.register(media_type, :json_api)
-  end
-
 end
