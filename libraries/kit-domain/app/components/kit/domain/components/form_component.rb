f42b2f0f1955addff891bfaef194231b167bd986
Nathan Appere <nathan.appere@gmail.com>
2021-07-08 10:45:21 +0200

    Kit::Domain - Add Form component

---
+++ libraries/kit-domain/app/components/kit/domain/components/form_component.rb
@@ -0,0 +1,46 @@
+# Shared logic for any Domain Component
+class Kit::Domain::Components::FormComponent < Kit::Domain::Components::Component
+
+  attr_reader :form_action, :form_method, :model, :csrf_token
+
+  def initialize(form_action:, form_method:, model:, csrf_token: nil, **)
+    super
+
+    @form_action = form_action
+    @form_method = form_method
+    @csrf_token  = csrf_token
+    @model       = model
+
+    @errors_list.each do |el|
+      if el[:detail].include?('$attribute') && el[:attribute]
+        el[:detail].gsub!('$attribute', el[:attribute].to_s)
+      end
+    end
+  end
+
+  def generic_errors
+    @generic_errors ||= errors_list
+      .reject { |el| fields_name.include?(el[:attribute]) } || []
+  end
+
+  def errors_by_field
+    if !defined?(@_errors_by_field)
+      list = errors_list.group_by { |v| v[:attribute] }
+      fields_name.each do |field|
+        if !list[field]
+          list[field] = []
+        end
+      end
+
+      @_errors_by_field = list
+    end
+
+    @_errors_by_field
+  end
+
+  def fields_name
+    return []
+    #raise 'IMPLEMENT ME'
+  end
+
+end
