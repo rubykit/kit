7e66776cb087ce33ffd9498f34a788058cdf6efc
Nathan Appere <nathan.appere@gmail.com>
2021-09-01 12:05:32 +0200

    Kit::Engine - Add public directory of engine when it exists

--- libraries/kit-engine/lib/kit/engine.rb
+++ libraries/kit-engine/lib/kit/engine.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module Kit # rubocop:disable Style/Documentation
 end
 
@@ -11,9 +13,16 @@ module Kit::Engine
     context.config.assets.paths     << File.expand_path('../../../../app/components', file)
     context.config.eager_load_paths << File.expand_path('../../../app/controllers',   file)
 
-    #context.config.paths.add 'app/components', eager_load: true
+    engine_public_path = File.expand_path('../../../../public', file)
+    if File.directory?(engine_public_path)
+      context.initializer 'static assets' do |app|
+        app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, engine_public_path)
+      end
+    end
 
 =begin
+    context.config.paths.add 'app/components', eager_load: true
+
     initializer_name = "#{ namespace.name.underscore.gsub('/', '_') }_engine"
     context.initializer initializer_name do
       ::ActiveAdmin.application.load_paths += Dir[File.expand_path('../../../app/admin', file)]
