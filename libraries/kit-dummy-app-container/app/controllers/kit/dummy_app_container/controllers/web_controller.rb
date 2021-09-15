63cbc32e5b115d0ffc6a0d721b601873545be26c
Nathan Appere <nathan.appere@gmail.com>
2021-09-15 18:49:21 +0200

    Kit::DummyAppContainer - Update for Router's router_conn change

--- libraries/kit-dummy-app-container/app/controllers/kit/dummy_app_container/controllers/web_controller.rb
+++ libraries/kit-dummy-app-container/app/controllers/kit/dummy_app_container/controllers/web_controller.rb
@@ -22,17 +22,17 @@ class Kit::DummyAppContainer::Controllers::WebController < ::ActionController::B
 
     _status, ctx = Kit::Organizer.call(
       list: [
-        Kit::Router::Adapters::HttpRails::Request::Import.method(:import_request),
-        ->(router_request:) { router_request.endpoint[:callable].call(router_request: router_request) },
+        Kit::Router::Adapters::HttpRails::Conn::Import.method(:import_request),
+        ->(router_conn:) { router_conn.endpoint[:callable].call(router_conn: router_conn) },
       ],
       ctx:  controller_ctx,
     )
 
     Kit::Organizer.call(
       list: [
-        Kit::Router::Adapters::HttpRails::Request::Export.method(:export_request),
+        Kit::Router::Adapters::HttpRails::Conn::Export.method(:export_response),
       ],
-      ctx:  controller_ctx.merge(ctx.slice(:router_request, :router_response)),
+      ctx:  controller_ctx.merge(ctx.slice(:router_conn)),
     )
 
     return
