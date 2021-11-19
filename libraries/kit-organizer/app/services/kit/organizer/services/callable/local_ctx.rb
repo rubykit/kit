3662268fed4a578d495b2d833523a09525f59907
Nathan Appere <nathan.appere@gmail.com>
2021-11-19 11:30:21 +0100

    Kit::Organizer - Update :local_ctx callable to support dynamic ctx keys renaming

--- libraries/kit-organizer/app/services/kit/organizer/services/callable/local_ctx.rb
+++ libraries/kit-organizer/app/services/kit/organizer/services/callable/local_ctx.rb
@@ -1,5 +1,11 @@
 # Allows you to enrich context of a specific callable, without leaking the value in the general context.
 #
+# The third argument is a local context that is injected as is.
+#
+# The 4th argument is a "rename" context. It renames keys from the ctx to be sent with another name to the callable.
+#   
+#
+#
 # Useful for custom error messages.
 #
 # ## Example
@@ -23,24 +29,54 @@ module Kit::Organizer::Services::Callable::LocalCtx
   # Receive element from `:list` and resolve it to a callable if the contract matches.
   #
   # The expected format for `:args` is `[:local_ctx, callable, local_context]`
-  before Ct::Hash[args: Ct::Array[Ct::Eq[:local_ctx], Ct::Any, Ct::Hash]]
+  before Ct::Hash[args: Ct::Array[Ct::Eq[:local_ctx], Ct::Any, Ct::Optional[Ct::Hash], Ct::Optional[Ct::Hash]]]
   def self.resolve(args:)
-    _, callable, local_ctx = args
+    _, callable, local_ctx, rename_rules = args
+
+    local_ctx    ||= {}
+    rename_rules ||= {}
 
     wrapped_callable = ->(**arguments) do
-      ctx_in_extended = (arguments || {}).merge(local_ctx)
+      renamed_ctx = {}
+      if rename_rules
+        res = rename_keys(ctx: arguments, rename_rules: rename_rules)
+        renamed_ctx = res[1][:renamed_ctx]
+      end
+
+      ctx_in_extended = (arguments || {})
+        .merge(local_ctx)
+        .merge(renamed_ctx)
 
       status, ctx_out = Kit::Organizer.call(
         list: [callable],
         ctx:  ctx_in_extended,
       )
 
-      ctx_out = ctx_out.except(*local_ctx.keys)
+      ctx_out = ctx_out
+        .except(*local_ctx.keys)
+        .except(*renamed_ctx.keys)
 
       [status, ctx_out]
     end
 
     [:ok, callable: wrapped_callable]
+  end
+
+  # Replace keys name in `:ctx` according to `:rename_rules`
+  #
+  # `:rename_rules` is a hash where the key are the new key_name, and the values the current key_name
+  def self.rename_keys(ctx:, rename_rules:)
+    renamed_ctx = {}
+
+    if rename_rules && !rename_rules.empty?
+      rename_rules.each do |k, v|
+        if ctx[v]
+          renamed_ctx[k] = ctx[v]
+        end
+      end
+    end
+
+    [:ok, renamed_ctx: renamed_ctx]
   end
 
 end
--- libraries/kit-organizer/spec/organizer/callable/local_ctx_spec.rb
+++ libraries/kit-organizer/spec/organizer/callable/local_ctx_spec.rb
@@ -4,19 +4,19 @@ require_relative '../../../lib/kit/organizer'
 describe Kit::Organizer::Services::Callable::LocalCtx do
 
   let(:callable) do
-    ->(a:, b:) { [:ok, a: a + b] }
+    ->(a:, b:, c:) { [:ok, a: a + b + c] }
   end
 
   let(:payload) do
-    [:local_ctx, callable, { b: 2 }]
+    [:local_ctx, callable, { b: 2 }, { c: :d }]
   end
 
   let(:ctx_in) do
-    { a: 1 }
+    { a: 1, d: 4 }
   end
 
   let(:ctx_out) do
-    { a: 3 }
+    { a: 7, d: 4 }
   end
 
   describe '.resolve' do
@@ -30,17 +30,87 @@ describe Kit::Organizer::Services::Callable::LocalCtx do
   end
 
   describe 'Kit::Organizer flow' do
-    it 'does not leak the local context into the general context' do
-      status, ctx = Kit::Organizer.call(
-        ok:  [
-          payload,
-        ],
-        ctx: ctx_in,
-      )
 
-      expect(status).to be :ok
-      expect(ctx).to eq(ctx_out)
+    context 'with both static & dynamic args provided' do
+
+      it 'does not leak the local context into the general context' do
+        status, ctx = Kit::Organizer.call(
+          ok:  [
+            payload,
+          ],
+          ctx: ctx_in,
+        )
+
+        expect(status).to be :ok
+        expect(ctx).to eq(ctx_out)
+      end
+
     end
+
+    context 'with only static arguments provided' do
+
+      let(:callable) do
+        ->(a:, b:) { [:ok, a: a + b] }
+      end
+
+      let(:payload) do
+        [:local_ctx, callable, { b: 2 }]
+      end
+
+      let(:ctx_in) do
+        { a: 1 }
+      end
+
+      let(:ctx_out) do
+        { a: 3 }
+      end
+
+      it 'works ' do
+        status, ctx = Kit::Organizer.call(
+          ok:  [
+            payload,
+          ],
+          ctx: ctx_in,
+        )
+
+        expect(status).to be :ok
+        expect(ctx).to eq(ctx_out)
+      end
+
+    end
+
+    context 'with only dynamic arguments provided' do
+
+      let(:callable) do
+        ->(a:, c:) { [:ok, a: a + c] }
+      end
+
+      let(:payload) do
+        [:local_ctx, callable, nil, { c: :d }]
+      end
+
+      let(:ctx_in) do
+        { a: 1, d: 4 }
+      end
+
+      let(:ctx_out) do
+        { a: 5, d: 4 }
+      end
+
+      it 'works ' do
+        status, ctx = Kit::Organizer.call(
+          ok:  [
+            payload,
+          ],
+          ctx: ctx_in,
+        )
+
+        expect(status).to be :ok
+        expect(ctx).to eq(ctx_out)
+      end
+
+    end
+
   end
 
 end
