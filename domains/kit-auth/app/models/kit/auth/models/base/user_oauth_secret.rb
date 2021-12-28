ac94da355b747cbfb005ed0e4f2f08df955299ec
Nathan Appere <nathan.appere@gmail.com>
2021-12-28 10:46:43 +0100

    Kit::Auth - Fix Lockbox code reload issue in models

--- domains/kit-auth/app/models/kit/auth/models/base/user_oauth_secret.rb
+++ domains/kit-auth/app/models/kit/auth/models/base/user_oauth_secret.rb
@@ -1,3 +1,5 @@
+require 'lockbox'
+
 module Kit::Auth::Models::Base::UserOauthSecret
 
   extend ActiveSupport::Concern
@@ -7,11 +9,14 @@ module Kit::Auth::Models::Base::UserOauthSecret
 
     acts_as_paranoid
 
-    # TODO: fix this, issues on code reload
-    if self.respond_to?(:encrypts)
-      encrypts :token,         encrypted_attribute: :secret_token
-      encrypts :refresh_token, encrypted_attribute: :secret_refresh_token
-    end
+    # NOTE: needed for dev mode code reload.
+    extend ::Lockbox::Model
+    #extend Lockbox::Model::Attached
+    singleton_class.send(:alias_method, :encrypts, :lockbox_encrypts) if ActiveRecord::VERSION::MAJOR < 7
+    ActiveRecord::Relation.prepend ::Lockbox::Calculations
+
+    encrypts :token,         encrypted_attribute: :secret_token
+    encrypts :refresh_token, encrypted_attribute: :secret_refresh_token
 
     read_columns = []
 
