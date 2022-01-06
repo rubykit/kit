8accba4d953681648f68a4bdf0862f1d9bed731d
Nathan Appere <nathan.appere@gmail.com>
2022-01-06 10:52:27 +0100

    Kit::ViewComponents - Add support for boolean as string & disabling vertical mode in InputRadioGroupComponent

--- libraries/kit-view_components/app/components/kit/view_components/components/form/input_radio_group_component.rb
+++ libraries/kit-view_components/app/components/kit/view_components/components/form/input_radio_group_component.rb
@@ -1,22 +1,38 @@
 class Kit::ViewComponents::Components::Form::InputRadioGroupComponent < Kit::ViewComponents::Components::Form::InputComponent
 
-  attr_reader :values
+  attr_reader :values, :vertical
 
-  def initialize(*, values:,  **)
+  def initialize(*, values:, vertical: nil, **)
     super
 
-    @values = values
+    @values   = values
+    @vertical = vertical || false
   end
 
+  # TODO: audit the ".to_s"?
   def list_values
     values.each_with_index.map do |el, idx|
       {
         label:    el[1] || el[0],
         value:    el[0],
-        selected: el[0] == value,
+        selected: el[0] == value || el[0] == value.to_s,
         input_id: "#{ input_id }_#{ idx }",
       }
     end
+  end
+
+  def input_group_class
+    list = []
+
+    if errors_list.size > 0
+      list << 'is-invalid'
+    end
+
+    if vertical
+      list << 'input-group-vertical'
+    end
+
+    list.join(' ')
   end
 
 end
