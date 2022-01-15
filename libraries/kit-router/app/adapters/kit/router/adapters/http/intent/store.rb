b1c95d32dcd4bf0ee64a91a4396daab88eeb8f3b
Nathan Appere <nathan.appere@gmail.com>
2022-01-15 15:08:21 +0100

    Kit::Router - Fix Intent strategies

--- libraries/kit-router/app/adapters/kit/router/adapters/http/intent/store.rb
+++ libraries/kit-router/app/adapters/kit/router/adapters/http/intent/store.rb
@@ -1,13 +1,7 @@
 module Kit::Router::Adapters::Http::Intent::Store
 
   def self.create_intent_store
-    {
-      steps: [
-        :user_sign_in,
-        :user_sign_up,
-      ],
-      types: {},
-    }
+    {}
   end
 
   def self.default_intent_store
--- libraries/kit-router/app/adapters/kit/router/adapters/http/intent/strategies.rb
+++ libraries/kit-router/app/adapters/kit/router/adapters/http/intent/strategies.rb
@@ -1,24 +1,34 @@
-module Kit::Router::Adapters::Http::Intent::Strategies::Parameter
+module Kit::Router::Adapters::Http::Intent::Strategies
 
-  def self.parameter_strategy
+  def self.save_url_and_redirect
     stategy = {
-      get: ->(router_conn:, **) { [:ok, intent_value: router_conn.request[:params][:intent]] },
+      get: ->(router_conn:, **) { [:ok, intent_value: router_conn[:request][:http][:cgi]['REQUEST_PATH']] },
       use: ->(router_conn:, intent_value:, **) do
-        # Do something based on the value that was saved.
+        Kit::Domain::Endpoints::Http.redirect_to(
+          router_conn: router_conn,
+          location:    intent_value,
+        )
       end,
     }
 
     [:ok, intent_strategy: stategy]
   end
 
-  def self.redirect_back
+  def self.save_url_and_set_redirect_url
+    stategy = {
+      get: ->(router_conn:, **)                { [:ok, intent_value: router_conn[:request][:http][:cgi]['REQUEST_PATH']] },
+      use: ->(router_conn:, intent_value:, **) { [:ok, redirect_url: intent_value] },
+
+    }
+
+    [:ok, intent_strategy: stategy]
+  end
+
+  def self.parameter_strategy
     stategy = {
       get: ->(router_conn:, **) { [:ok, intent_value: router_conn.request[:params][:intent]] },
       use: ->(router_conn:, intent_value:, **) do
-        Kit::Domain::Endpoints::Http.redirect_to(
-          router_conn: router_conn,
-          location:    intent_value,
-        )
+        # Do something based on the value that was saved.
       end,
     }
 
