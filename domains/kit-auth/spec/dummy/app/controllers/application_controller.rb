ee536d44fdb5221d9a4ebf3c5672331b50971b8c
Nathan Appere <nathan.appere@gmail.com>
2022-01-26 18:56:25 +0100

    Kit::Auth - Add some context for weird inheritance scheme in dummy app

--- domains/kit-auth/spec/dummy/app/controllers/application_controller.rb
+++ domains/kit-auth/spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 # NOTE: AA implicit dependency (needed for `inherited-ressources` to work)
+# Ref: https://github.com/activeadmin/inherited_resources/issues/618
 class ::ApplicationController < Kit::Auth::DummyAppContainer::Controllers::WebController
 end
