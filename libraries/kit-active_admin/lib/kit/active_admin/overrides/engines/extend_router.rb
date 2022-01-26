c3c2bfe3ea5f3b7e9b8b03e9ffe38cf31a90059f
Nathan Appere <nathan.appere@gmail.com>
2022-01-26 18:38:56 +0100

    Kit::ActiveAdmin - Fix root_to routes

--- libraries/kit-active_admin/lib/kit/active_admin/overrides/engines/extend_router.rb
+++ libraries/kit-active_admin/lib/kit/active_admin/overrides/engines/extend_router.rb
@@ -2,7 +2,30 @@ class ActiveAdmin::Router
 
   private
 
-  # Allow to override the way routes are mounted through namespace config.
+  # Allow to override the way the root_to is mounted through external namespace config.
+  #
+  # ### References
+  # - https://github.com/activeadmin/activeadmin/blob/v2.9.0/lib/active_admin/router.rb#L18
+  def define_root_routes
+    namespaces.each do |namespace|
+      if namespace.root?
+        router.root namespace.root_to_options.merge(to: namespace.root_to)
+      else
+        options = namespace.route_options.dup
+        options.merge!({
+          as:     namespace.scope_as,
+          path:   namespace.scope_path,
+          module: namespace.scope_module,
+        })
+
+        router.scope(options) do
+          router.root namespace.root_to_options.merge(to: namespace.root_to, as: :root)
+        end
+      end
+    end
+  end
+
+  # Allow to override the way routes are mounted through external namespace config.
   #
   # ### References
   # - https://github.com/activeadmin/activeadmin/blob/v2.9.0/lib/active_admin/router.rb#L108
@@ -14,7 +37,7 @@ class ActiveAdmin::Router
       module: config.namespace.scope_module,
     })
 
-    router.scope options do
+    router.scope(options) do
       define_routes(config)
     end
   end
