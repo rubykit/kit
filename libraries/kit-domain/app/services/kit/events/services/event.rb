69c4ee7526c0cd8044f1d0a966adfb10ef2378a2
Nathan Appere <nathan.appere@gmail.com>
2022-02-01 18:07:24 +0100

    Kit::Domain - Cast event_model|created event when creating an Event model

--- libraries/kit-domain/app/services/kit/events/services/event.rb
+++ libraries/kit-domain/app/services/kit/events/services/event.rb
@@ -1,6 +1,22 @@
 module Kit::Events::Services::Event
 
-  def self.create_event(name:, data:, metadata: nil)
+  # Persist event & fire a generic event_model|created event
+  def self.persist_event(name:, data:, metadata: nil)
+    Kit::Organizer.call(
+      list: [
+        self.method(:create_event_model),
+        self.method(:send_event_model_created_event),
+      ],
+      ctx:  {
+        name:     name,
+        data:     data,
+        metadata: metadata,
+      },
+    )
+  end
+
+  # Persist the event in a database
+  def self.create_event_model(name:, data:, metadata: nil)
     metadata ||= {}
 
     event_model = Kit::Events::Models::Write::Event.create({
@@ -10,6 +26,19 @@ module Kit::Events::Services::Event
     })
 
     [:ok, event_model: event_model]
+  end
+
+  # Fire a generic event_model|created event
+  def self.send_event_model_created_event(event_model:)
+    Kit::Router::Services::Adapters.cast(
+      route_id:     'event|event_model|created',
+      adapter_name: :async,
+      params:       {
+        event_model_id: event_model.id,
+      },
+    )
+
+    [:ok]
   end
 
 end
