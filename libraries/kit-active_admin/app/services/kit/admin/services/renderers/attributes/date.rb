ecca562bf305618a55d6f75b72dbcdd505a54bc8
Nathan Appere <nathan.appere@gmail.com>
2022-02-16 16:26:20 +0100

    Kit::Admin - Fix date attribute

--- libraries/kit-active_admin/app/services/kit/admin/services/renderers/attributes/date.rb
+++ libraries/kit-active_admin/app/services/kit/admin/services/renderers/attributes/date.rb
@@ -3,7 +3,7 @@ module Kit::Admin::Services::Renderers::Attributes::Date
   def self.call(el:, ctx:, functor:, **)
     value = functor.call(el)
 
-    ctx.color_tag value
+    ctx.date_tag value
   end
 
 end
