3f4dbce5f8667626860a7a7ca7eaf595d79a18bc
Nathan Appere <nathan.appere@gmail.com>
2022-03-07 17:24:12 +0100

    Kit::Admin - Add support for multi color tags

--- libraries/kit-active_admin/app/services/kit/admin/services/renderers/attributes/color.rb
+++ libraries/kit-active_admin/app/services/kit/admin/services/renderers/attributes/color.rb
@@ -3,7 +3,13 @@ module Kit::Admin::Services::Renderers::Attributes::Color
   def self.call(el:, ctx:, functor:, **)
     value = functor.call(el)
 
-    ctx.color_tag value
+    if value.is_a?(Array)
+      value.each do |v|
+        ctx.color_tag v
+      end
+    else
+      ctx.color_tag value
+    end
   end
 
 end
