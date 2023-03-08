2e6403f944e215850fa3b7f0a34a55f69efdd319
Nathan Appere <nathan.appere@gmail.com>
2023-03-08 10:45:08 +0100

    Kit::Auth - Add sidekiq delayed gem

--- libraries/kit-dummy_app_container/config/initializers/sidekiq.rb
+++ libraries/kit-dummy_app_container/config/initializers/sidekiq.rb
@@ -13,5 +13,3 @@ schedule_file = 'config/sidekiq_cron.yml'
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
-
-Sidekiq::Extensions.enable_delay!
